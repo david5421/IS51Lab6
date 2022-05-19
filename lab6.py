1f2633e834c637a68269fc60331f4aebb08ebd4b
David Bautista <davidbautista@swuser-129-8-219-130.swlan.csufresno.edu>
2022-05-19 14:05:11 -0700

    adding pseudocode

--- lab6.py
+++ lab6.py
@@ -15,3 +15,27 @@ otherwise the number is not any of the above, then the result will be "Bar"
 we iterate over using a loop times and print the results to the user. As an example "Plum Cherries Melon"
 
 """
+
+"""
+num = generate random number
+
+if the number is greater than 15,
+ then the result will be "Cherries"
+otherwise if the number is > 10,
+  then the result will be "Orange
+otherwise if the number is > 5,
+  then the result will be "Plum"
+otherwise if the number is > 2,
+  then the result will be "Melon"
+otherwise if the number is > 1, 
+  then the result will be "Bell"
+otherwise
+  then the result will be "Bar"
+
+loop three times
+    print the output (fruit) to the user
+
+"""
+
+
+
